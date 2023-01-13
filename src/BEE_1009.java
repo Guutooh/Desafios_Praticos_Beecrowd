6db41bd7653bab2048713750a9cd568fd2c8fb4f
Guutooh <gustavo.oliveiradossantos@gmail.com>
2023-01-13 11:21:14 -0300

    Desafio 1009

---
+++ src/BEE_1009.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BEE_1009 {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+
+        String nomeVendedor = input.next();
+        double salarioFixo = input.nextDouble();
+        double totalVendas = input.nextDouble();
+
+        double TOTAL = ((totalVendas*15)/100) + salarioFixo;
+
+        System.out.println(String.format("TOTAL = R$ %.2f" , TOTAL));
+
+        input.close();
+
+
+    }
+
+
+}
